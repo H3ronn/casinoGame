87114bb5662f45cbbc19d80c976c5d0a8c1f03c1
Bartosz Czaplicki <h3ronnn@gmail.com>
2021-08-14 18:56:36 +0200

    add animation on click button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-class Game {
-    amount = 3;
+// class Game {
+//     amount = 3;
 
-    constructor() {
+//     constructor() {
 
-    }
-}
+//     }
+// }
+
+const cube = document.querySelector('.cubeContainer');
+const playButton = document.querySelector('.playButton')
+
+playButton.addEventListener('click', () => {cube.style.animation = 'roll 5s ease-in-out';} );
